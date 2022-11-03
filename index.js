18c2e9cf9c5569841fda2cf0ae7c354bdb431c91
andrew <andrewasmit@hotmail.com>
2022-11-02 20:04:10 -0500

    commit tree dd701e182b9a9d98071a64cb71f9aaf920835adc

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`,
+        }),
+    })
+    .then(res => res.json())
+    .then(data=>idHandler(data))
+    .catch(error => errorHandler(error));
+}
+
+function idHandler(e){
+    let post = document.createElement('div');
+    post.innerHTML = e.id;
+    document.body.appendChild(post);
+}
+
+function errorHandler(e){
+    let msg = document.createElement('div');
+    msg.innerHTML = e.message;
+    document.body.appendChild(msg);
+}
+
+// submitData('andrew', 'andrewasmit');
